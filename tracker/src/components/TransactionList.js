330ba45a87f6199d23511d4db8d4fc2fc3680e90
kipkoech88 <hoseakipkoech88@gmail.com>
2023-10-20 23:06:15 +0300

    added Components

--- tracker/src/components/TransactionList.js
+++ tracker/src/components/TransactionList.js
@@ -5,7 +5,7 @@ function TransactionList() {
     <div>
       <h3>History</h3>
       <ul id='list' className='list'>
-        <li>Cash</li>
+        <li className="minus">Cash <span>$400</span> <button className='delete-btn'>X</button> </li>
       </ul>
     </div>
   )
